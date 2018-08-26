5b023cca883e427a9317bf5995098ad39a8406b3
Ekaterina Goranova <epgoranova@gmail.com>
2018-08-26 21:05:17 +0200

    setup: link config files within directories

--- setup.sh
+++ setup.sh
@@ -2,32 +2,53 @@
 
 DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Create symlinks to the dotfiles.
+link() {
+    SRC=$1
+    DST=$2
+
+     if [ -e "$DST" ]; then
+         if [ -L "$DST" ]; then
+              read -r -p "Target $DST is a symlink. Do you want to replace it? (y/n) "
+              if [[ $REPLY =~ ^[^Yy]$ ]]; then
+                  return
+              fi;
+          else
+              read -r -p "Target $DST exists and is not a symlink. Do you want to replace it? (y/n) "
+              if [[ $REPLY =~ ^[^Yy]$ ]]; then
+                  return
+              fi;
+         fi
+     fi
+
+    ln -sf "$SRC" "$DST"
+    echo "Created symlink $DST"
+}
+
+# create symlinks to the dotfiles
 
 for DIR in editor git shell; do
-    for TEMPLATE in "$DOTFILES/$DIR"/*; do
-        TARGET="$HOME/.$(basename $TEMPLATE)"
+    for SOURCE in "$DOTFILES/$DIR"/*; do
+        TARGET="$HOME/.$(basename $SOURCE)"
 
-        if [ -e "$TARGET" ]; then
-            if [ -L "$TARGET" ]; then
-                read -r -p "Target $TARGET is a symlink. Do you want to replace it? (y/n) "
-                if [[ $REPLY =~ ^[^Yy]$ ]]; then
-                    continue
-                fi;
-            else
-                read -r -p "Target $TARGET exists and is not a symlink. Do you want to replace it? (y/n) "
-                if [[ $REPLY =~ ^[^Yy]$ ]]; then
-                    continue
-                fi;
-            fi
+        if [ -f "$SOURCE" ]; then
+            link "$SOURCE" "$TARGET"
+
+        elif [ -d "$SOURCE" ]; then
+            pushd $SOURCE > /dev/null
+
+            # recreate directory structure without the files
+            find . -type d -exec mkdir -p -- $TARGET/{} \;
+
+            for FILE in $(find . -type f); do
+                link "$PWD/${FILE#./}" "$TARGET/${FILE#./}"
+            done
+
+            popd > /dev/null
         fi
-
-        ln -sf "$TEMPLATE" "$TARGET"
-        echo "Created $TARGET symlink"
     done
 done
 
-# Setup scripts.
+# setup scripts
 
 BASH_SCRIPTS="$HOME/.bash"
 if [ ! -d "$BASH_SCRIPTS" ]; then
